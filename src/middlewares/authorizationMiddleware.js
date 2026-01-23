6d99b2ee945e602ace598f357ae4f0e04bb63cec
lzanetti <lzanetti@onit.it>
2026-01-23 14:49:37 +0100

    fix: revert to using only auth conf

--- src/middlewares/authorizationMiddleware.js
+++ src/middlewares/authorizationMiddleware.js
@@ -27,8 +27,8 @@ exports.authorize = async (req, res, next) => {
 };
 
 exports.socketAuthorize = async (socket, next) => {
-    const token = socket.handshake.auth.token || socket.handshake.query.token; 
-    
+    const token = socket.handshake.auth.token; 
+
     log("[DEBUG] Token: " + token);
     if (!token) {
         return res.sendStatus(401);
